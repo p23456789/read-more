4cb8c1e307696d79b2166a92ab73d038074b9e4f
Paul Wong-Gibbs <paul.wong-gibbs@indigotree.co.uk>
2025-11-01 19:30:00 +0000

    Only load wpcom-vip-cli-command mock if it doesn't exist.

--- includes/class-wpcom-vip-cli-command.php
+++ includes/class-wpcom-vip-cli-command.php
@@ -3,6 +3,10 @@ declare(strict_types=1);
 
 defined( 'ABSPATH' ) || exit;
 
+if ( class_exists( 'WPCOM_VIP_CLI_Command' ) ) {
+	return;
+}
+
 /**
  * Hi, reviewers! I do not have a local WP VIP environment, so I'm using this class to extend the
  * WP_CLI_Command class in order to silence the
